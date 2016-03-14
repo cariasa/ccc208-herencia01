9cb84714a48449768888f647870cf2336ebd4cb3
Carlos Arias <cariasa@unitec.edu>
2016-03-14 09:15:24 -0600

    student.h

---
+++ student.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "person.h"
+#include <string>
+using std::string;
+
+class Student : public Person{
+	string id;
+	string major;
+	double gpa;
+	int uv;
+  public:
+	Student(string, string, int, string, string="None", double=0.0, int=0);
+	Student(const Student&);
+	~Student();
+	string toString()const;
+	void addGrade(double, int);
+	double getGpa()const;
+	void setMajor(string);
+};
